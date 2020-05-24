f2fbb69ce9cb6c21205f077c486fe608d2edeee3
Anan Roderick <thamarnan@gmail.com>
2020-05-23 20:23:02 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
+#Run on libreelec console
 
 cd /storage/.kodi/media/Fonts
 wget https://github.com/thamarnan/libreelec_setup/raw/master/ArialUnicode.ttf
+sed -i '/subtitles.font/c\<setting id="subtitles.font">ArialUnicode.ttf</setting>' /storage/.kodi/userdata/guisettings.xml
+
+
